61949c7b1b9aa6662c6b777d480109c2445b2115
dedgar1 <jnzokwe@gmail.com>
2015-01-18 20:54:36 -0500

    Turns bluetooth on if it is turned off

--- app/src/main/java/com/eieio/edgar/fmd/MainActivity.java
+++ app/src/main/java/com/eieio/edgar/fmd/MainActivity.java
@@ -1,17 +1,36 @@
 package com.eieio.edgar.fmd;
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    private BluetoothAdapter mBluetoothAdapter;
+    private SparseArray<BluetoothDevice> mDevices;
+    private ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setProgressBarIndeterminate(true);
+        BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        mBluetoothAdapter = manager.getAdapter();
+        mDevices = new SparseArray<BluetoothDevice>();
+        mProgress = new ProgressDialog(this);
+        mProgress.setIndeterminate(true);
+        mProgress.setCancelable(false);
+
     }
 
 
@@ -20,6 +39,21 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    protected void onResume(){
+        super.onResume();
+        if(mBluetoothAdapter ==null || !mBluetoothAdapter.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBtIntent);
+            //finish();
+
+        }
+        //check for btLE support
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)){
+            Toast.makeText(this,"No LE support.", Toast.LENGTH_SHORT).show();
+
+        }
     }
 
     @Override
